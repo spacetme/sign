1fe43fc4e5af139e8bf039ec8bdb4738729b6700
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-02-08 12:58:35 +0700

    more angular! and more verbose, travis?

--- source/javascripts/sign.js
+++ source/javascripts/sign.js
@@ -1,3 +1,16 @@
 
+
 angular.module('sign', ['ui.slider'])
+.controller('SettingsController', function($scope) {
+
+  $scope.settings = {
+    content: 'text',     /* type: text/clock */
+    text: 'Attention',   /* (text) text to display */
+    rate: 100            /* display flashing rate */
+  }
+
+})
+
+
+
 

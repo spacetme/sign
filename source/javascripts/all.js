16b571779d8be715053a2dc495233146acb548da
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-02-08 12:37:25 +0700

    I WASTED ONE HOUR TRYING TO FIND A GOOD SLIDER PLUGIN FOR ANGULAR...

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,13 @@
 
+//= require jquery/jquery
+//= require jquery-ui/ui/jquery.ui.core.js
+//= require jquery-ui/ui/jquery.ui.widget.js
+//= require jquery-ui/ui/jquery.ui.mouse.js
+//= require jquery-ui/ui/jquery.ui.slider.js
+//= require jquery-ui-touch-punch/jquery.ui.touch-punch
+//= require modernizr/modernizr
+//= require angular/angular
+//= require angular-touch/angular-touch
+//= require angular-ui-slider/src/slider
+//
+//= require sign

6ee2c0feb8627cd16c90b7cebe09b356ffe44cf9
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2014-02-09 00:02:33 +0700

    add app cache

--- config.rb
+++ config.rb
@@ -33,6 +33,9 @@ require 'ngmin/processor'
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+proxy "/online.html", "/template.html", :locals => { :offline => false }, :ignore => true
+proxy "/index.html", "/template.html", :locals => { :offline => true }, :ignore => true
+
 ###
 # Helpers
 ###
